bfcf63ed5784a18ee55d1cd9d7d66f65f0f909b5
CharlieYu <yuhangbinccc@gmail.com>
2025-02-26 12:21:14 +0800

    feat: factory method pattern

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # design-pattern
+
+## creational pattern
+
+### factory method
+Provide an interface for creating objects in a superclass, but allow subclasses to alter the type of objects that will be created.
+
+
+
+### abstract factory
+Provide an interface for creating families of related or dependent objects without specifying their concrete classes.
+
+
+
+## structural pattern
+
+
+## behavioral pattern
+
+
---
+++ creational/factory_method.py
@@ -0,0 +1,47 @@
+from abc import ABC, abstractmethod
+
+# create a transport interface with a method to deliver
+
+class Transport(ABC):
+    @abstractmethod
+    def deliver(self):
+        pass
+
+# implement the transport interface for a Truck and Ship
+
+class Truck(Transport):
+    def deliver(self):
+        return "Delivering by truck"
+
+class Ship(Transport):
+    def deliver(self):
+        return "Delivering by ship"
+# create a logistic interface with a method to create a transport
+
+class Logistic:
+    @abstractmethod
+    def create_transport(self):
+        pass
+
+# implement the logistic interface for a RoadLogistic and SeaLogistic
+
+class RoadLogistic(Logistic):
+    def create_transport(self):
+        return Truck()
+
+class SeaLogistic(Logistic):
+    def create_transport(self):
+        return Ship()
+
+
+# main function and run the program
+
+def main():
+    road_logistic = RoadLogistic()
+    sea_logistic = SeaLogistic()
+    print(road_logistic.create_transport().deliver())
+    print(sea_logistic.create_transport().deliver())
+
+if __name__ == "__main__":
+    main()
+
